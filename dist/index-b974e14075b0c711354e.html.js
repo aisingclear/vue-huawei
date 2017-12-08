a921d9612d3ac7f2192e116692d88e2e6796eab4
aisingclear <596871226.com>
2017-12-08 22:42:00 +0800

    html-webpack-plugin2

--- dist/index-b974e14075b0c711354e.html.js
+++ dist/index-b974e14075b0c711354e.html.js
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>we to good1www</title>
+    <title>we to good</title>
 <script type="text/javascript" src="js/main-34a97384840fdea6c87e.js"></script></head>
 <body>
 </body>
